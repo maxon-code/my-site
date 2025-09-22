66dd868eb5b428116d0d2ebc31eeefe96f5fb8f3
Maksim <132435338+maxon-code@users.noreply.github.com>
2025-09-22 17:21:02 +0300

    site

--- src/projects/memory-game/types.ts
+++
@@ -1,6 +0,0 @@
-export interface Card {
-    id: number;
-    src: string;
-    matched: boolean;
-}
-
--- src/projects/weather-app/MainWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MainMap from './MainMap';
-
-export default function MainMapWrapper() {
-    return <MainMap />;
-}
